bb70d01d94c80eaf028d6b6cd16532a2e4e3e6d6
SalahaldinBilal <64901518+SalahaldinBilal@users.noreply.github.com>
2023-05-01 19:16:38 +0300

    try catch entire function

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import type { APIGatewayEvent } from 'aws-lambda';
+import type { APIGatewayEvent, APIGatewayProxyEvent } from 'aws-lambda';
 import * as dotenv from 'dotenv'
 import { uploadFileToS3 } from './helpers';
 import type { CsvFile } from './types';
@@ -10,30 +10,30 @@ const headers = {
   'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
 };
 
-export const handler = async (event: APIGatewayEvent) => {
-  const csvFileToUpload = event.body as CsvFile;
+export const handler = async (event: APIGatewayProxyEvent) => {
+  try {
+    const csvFileToUpload = event.body as CsvFile;
 
-  if (!csvFileToUpload?.length) {
+    if (!csvFileToUpload?.length) {
+      return {
+        statusCode: 400,
+        headers,
+        message: "Empty or non existent file"
+      }
+    }
+
+    await uploadFileToS3(csvFileToUpload);
     return {
-      statusCode: 400,
+      statusCode: 200,
       headers,
-      message: "Empty or non existent file"
+      message: "File uploaded successfully"
     }
   }
-
-  try {
-    await uploadFileToS3(csvFileToUpload);
-  } catch (error) {
+  catch (error) {
     return {
       statusCode: 500,
       headers,
       message: `Unexpected Error Happened: ${error}`
     }
-  }
-
-  return {
-    statusCode: 200,
-    headers,
-    message: "File uploaded successfully"
   }
 };
