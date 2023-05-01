eee97499e79ab73fe79011f4efff4510c75c9d84
SalahaldinBilal <64901518+SalahaldinBilal@users.noreply.github.com>
2023-05-01 18:50:38 +0300

    change key name

--- src/types.ts
+++ src/types.ts
@@ -2,8 +2,8 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       readonly BUCKET_NAME: string,
-      readonly AWS_ACCESS_KEY_ID: string,
-      readonly AWS_SECRET_ACCESS_KEY: string,
+      readonly S3_ACCESS_KEY_ID: string,
+      readonly S3_SECRET_ACCESS_KEY: string,
       readonly S3_REGION: string,
     }
   }
